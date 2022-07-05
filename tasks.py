c4e2ff3fc6e9b4009f6985e8c8922dd7314090e9
mtkennerly <mtkennerly@gmail.com>
2022-07-05 18:09:57 +0800

    Update for Linguini 0.3.0

--- tasks.py
+++ tasks.py
@@ -16,11 +16,6 @@ def get_version():
 
 @task
 def build(ctx):
-    if not (REPO / "vendor" / "Linguini").exists():
-        ctx.run("git clone https://github.com/Ygg01/Linguini.git vendor/Linguini")
-        with ctx.cd("vendor/Linguini"):
-            ctx.run("git checkout 9a7939b00c3ca68df19b45afc0e39bad05c0b483")
-
     ctx.run("dotnet build src -c Release")
 
 
